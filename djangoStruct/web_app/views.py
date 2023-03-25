215b4acf86cae8b23b08a11b99f843f063eeaa7e
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2023-03-24 23:26:06 -0600

    Rendering React View to initial home page

--- djangoStruct/web_app/views.py
+++ djangoStruct/web_app/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from . models import *
 from rest_framework.response import Response
 from . serializer import *
+from django.http import HttpResponse
 # Create your views here.
 
 class ReactView(APIView):
@@ -40,3 +41,13 @@ class ReactView(APIView):
     def options(self, request):
         # handle OPTIONS requests here
         pass
+
+
+# Views are just a function which gets a request and returns a response (called actions in other architectures)
+# Mapping of a view to a URL should take place so that each time a request is made to a URL, a particular function (view) is called
+def test(request):
+    '''
+    Recieves a HTTP Request, and returns an instance of a HTTP Response
+    '''
+    # This function is returning a template, which is a depreciated practice, can dynamically build the HTML File in the front end insead
+    return render(request, 'homepage.html')
