b45b349b5fe9879ffd3495a62486228e227e208a
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2023-04-01 00:18:12 -0600

    Remove displaced functions from web_app url.py

--- djangoStruct/web_app/urls.py
+++ djangoStruct/web_app/urls.py
@@ -5,6 +5,5 @@ from . import views
 # this is a special variable that Django looks for, it will be an array of URL pattern objects
 urlpatterns = [
     # the path function takes a URL, and a refrence to a views function and returns a URL pattern object
-    path('', views.home, name='homepage'),
-    path('test/', views.test_function) 
+    path('test/', views.test) 
 ]
