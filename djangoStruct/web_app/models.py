b31362544d46528b7641c16a9538e4b05dae2fd5
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2023-04-07 05:03:46 -0600

    Update data models to delete redundant User model

--- djangoStruct/web_app/models.py
+++ djangoStruct/web_app/models.py
@@ -1,31 +1,21 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 # Models are the database objects
 
 
-class User(models.Model):
-    username = models.CharField(max_length=8, primary_key=True)
-    email = models.EmailField()
-    firstName = models.CharField(max_length=40)
-    lastName = models.CharField(max_length=40)
-    password = models.CharField(max_length=25)
-
-    def __str__(self):
-        return f"{self.username}; {self.email}; {self.firstName}; {self.lastName}; {self.password}"
-
-
 class Attendant(models.Model):
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
 
     def __str__(self):
         return self.username
 
 
 class Verifier(models.Model):
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
 
     def __str__(self):
         return self.username
@@ -40,7 +30,7 @@ class Coach(models.Model):
 
 
 class Tracked(models.Model):
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
     goal_hours = models.DecimalField(max_digits=7, decimal_places=2)
     
     def __str__(self):
@@ -81,14 +71,14 @@ class ActiveLivingFacility(models.Model):
 
 
 class TrackedSessions(models.Model):
-    tracked_username = models.ForeignKey(Tracked, on_delete=models.CASCADE)
-    # attendant_username = models.ForeignKey(Attendant, on_delete=models.CASCADE)
-    facility = models.ForeignKey(ActiveLivingFacility, on_delete=models.CASCADE)
+    tracked_username = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
+    # TODO Make this a foreign key to Active Living Facility when that table has been populated
+    facility_id = models.IntegerField()
     check_in_time = models.DateTimeField(default=datetime.now)
     check_out_time = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.tracked_username}; {self.facility}; {self.check_in_time}; {self.check_out_time}"
+        return f"{self.tracked_username}; {self.facility_id}; {self.check_in_time}; {self.check_out_time}"
 
 
 class Alumnus(models.Model):
