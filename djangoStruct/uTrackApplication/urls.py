69d4c7001a97e8ec0056c2df16a3e98442283887
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2023-04-09 16:57:57 -0600

    Add to ongoing session endpoint by adding deletes

--- djangoStruct/uTrackApplication/urls.py
+++ djangoStruct/uTrackApplication/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 	path('api/get_Users/', AllUsersView.as_view()),
 	path('api/Checkins/', CheckInSystemView.as_view()),
 	path('api/get_Checkins/<str:tracked_username>/', CheckInSystemView.as_view()),
-    path('api/get_Checkins/last/<str:tracked_username>/', CheckOutView.as_view()),
+    path('api/Checkout/last/<str:tracked_username>/', CheckOutView.as_view()),
     path('api/get_Classes/<str:tracked_username>', UserClassesView.as_view()),
     path('api/get_Equipment/<str:tracked_username>', UserEquipmentView.as_view()),
     path('api/get_Intramurals/<str:tracked_username>', UserIntramuralsView.as_view())
