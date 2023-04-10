9f1aa19bb060b01b29144d5d1515f9651f608346
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2023-04-09 22:48:08 -0600

    Bug Fix for Time Calculation

--- djangoStruct/utrackfront/src/LoginForm.js
+++ djangoStruct/utrackfront/src/LoginForm.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function LoginForm({ history }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [group, setGroup] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [group, setGroup] = useState('');
   const [isUser, setIsUser] = useState(true);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -18,41 +18,41 @@ function LoginForm({ history }) {
 
   const handleUserCheckboxChange = (event) => {
     setIsUser(true);
-    setGroup("Tracked");
+    setGroup('Tracked');
   };
 
   const handleAttendantCheckboxChange = (event) => {
     setIsUser(false);
-    setGroup("Attendant");
+    setGroup('Attendant');
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setError("");
+    setError('');
 
     axios
-      .post("http://127.0.0.1:8000/api/auth/login/", {
+      .post('http://127.0.0.1:8000/api/auth/login/', {
         username,
         password,
         group,
       })
       .then((response) => {
         if (!isUser) {
-          history.push("/attendant");
+          history.push('/attendant');
           history.go(0);
         } else {
-          history.push("/user");
+          history.push('/user');
           history.go(0);
         }
       })
       .catch((error) => {
         console.log(error);
-        setError("Invalid credentials. Please try again.");
+        setError('Invalid credentials. Please try again.');
       });
   };
 
   const handleRegistration = (event) => {
-    history.push("/register");
+    history.push('/register');
     history.go(0);
   };
 
@@ -61,20 +61,20 @@ function LoginForm({ history }) {
       <div style={styles.login}>
         <div style={styles.username}>
           <div>
-            <label htmlFor="username">UCID:</label>
+            <label htmlFor='username'>UCID:</label>
             <input
-              type="text"
-              id="username"
+              type='text'
+              id='username'
               value={username}
               onChange={handleUsernameChange}
               style={styles.input}
             />
           </div>
           <div>
-            <label htmlFor="password">Password:</label>
+            <label htmlFor='password'>Password:</label>
             <input
-              type="password"
-              id="password"
+              type='password'
+              id='password'
               value={password}
               onChange={handlePasswordChange}
               style={styles.input}
@@ -82,28 +82,28 @@ function LoginForm({ history }) {
           </div>
         </div>
         <div>
-          <label htmlFor="user-checkbox">
+          <label htmlFor='user-checkbox'>
             <input
-              type="checkbox"
-              id="user-checkbox"
+              type='checkbox'
+              id='user-checkbox'
               checked={isUser}
               onChange={handleUserCheckboxChange}
-            />{" "}
+            />{' '}
             User
           </label>
-          <label htmlFor="attendant-checkbox">
+          <label htmlFor='attendant-checkbox'>
             <input
-              type="checkbox"
-              id="attendant-checkbox"
+              type='checkbox'
+              id='attendant-checkbox'
               checked={!isUser}
               onChange={handleAttendantCheckboxChange}
-            />{" "}
+            />{' '}
             Attendant
           </label>
         </div>
         <div style={styles.errorMess}>{error}</div>
         <button onClick={() => handleRegistration()}>Register</button>
-        <button type="submit">Login</button>
+        <button type='submit'>Login</button>
       </div>
     </form>
   );
@@ -111,41 +111,41 @@ function LoginForm({ history }) {
 
 const styles = {
   errorMess: {
-    fontSize: "10px",
-    color: "red",
+    fontSize: '10px',
+    color: 'red',
   },
   appBackground: {
-    color: "#db471a",
+    color: '#db471a',
   },
   login: {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    backgroundColor: "#F08000",
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#F08000',
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     lineHeight: 2.0,
-    color: "#292b2c",
+    color: '#292b2c',
     borderRadius: 30,
-    borderWidth: "thick",
-    width: "35%",
-    height: "250px",
+    borderWidth: 'thick',
+    width: '35%',
+    height: '250px',
   },
   username: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "10px",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '10px',
   },
   input: {
-    boxSizing: "border-box",
-    width: "100%",
-    padding: "3px",
-    borderRadius: "5px",
-    border: "1px solid #ccc",
-    outline: "none",
+    boxSizing: 'border-box',
+    width: '100%',
+    padding: '3px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    outline: 'none',
   },
 };
 
