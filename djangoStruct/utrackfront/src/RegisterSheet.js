e62e8fa18439b6faf80e2b011ab57fee1d28d976
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2023-04-13 21:57:20 -0600

    Final changes for styling

--- djangoStruct/utrackfront/src/RegisterSheet.js
+++ djangoStruct/utrackfront/src/RegisterSheet.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import './App.css';
+
 function LoginForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [group, setGroup] = useState("Tracked");
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [group, setGroup] = useState('Tracked');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
   const history = useHistory();
 
   const handleUsernameChange = (event) => {
@@ -22,19 +24,19 @@ function LoginForm() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setError("");
+    setError('');
 
     if (
-      username === "" ||
-      password === "" ||
-      email === "" ||
-      firstName === "" ||
-      lastName === ""
+      username === '' ||
+      password === '' ||
+      email === '' ||
+      firstName === '' ||
+      lastName === ''
     ) {
-      setError("Make sure all fields are appropriately filled in.");
+      setError('Make sure all fields are appropriately filled in.');
     } else {
       axios
-        .post("http://127.0.0.1:8000/api/auth/register/", {
+        .post('http://127.0.0.1:8000/api/auth/register/', {
           username,
           password,
           email,
@@ -43,21 +45,21 @@ function LoginForm() {
           last_name: lastName,
         })
         .then((response) => {
-          setSuccess("Validation complete. You may now login.");
-          setUsername("");
-          setPassword("");
-          setFirstName("");
-          setLastName("");
-          setEmail("");
+          setSuccess('Validation complete. You may now login.');
+          setUsername('');
+          setPassword('');
+          setFirstName('');
+          setLastName('');
+          setEmail('');
         })
         .catch((error) => {
-          setError("Make sure all fields are appropriately filled in.");
+          setError('Make sure all fields are appropriately filled in.');
         });
     }
   };
 
   const handleLogout = (event) => {
-    history.push("/");
+    history.push('/');
     history.go(0);
   };
 
@@ -66,50 +68,50 @@ function LoginForm() {
       <div style={styles.login}>
         <div style={styles.username}>
           <div>
-            <label htmlFor="first_name">First Name:</label>
+            <label htmlFor='first_name'>First Name:</label>
             <input
-              type="text"
-              id="first_name"
+              type='text'
+              id='first_name'
               value={firstName}
               onChange={(event) => setFirstName(event.target.value)}
               style={styles.input}
             />
           </div>
           <div>
-            <label htmlFor="last_name">Last Name:</label>
+            <label htmlFor='last_name'>Last Name:</label>
             <input
-              type="text"
-              id="last_name"
+              type='text'
+              id='last_name'
               value={lastName}
               onChange={(event) => setLastName(event.target.value)}
               style={styles.input}
             />
           </div>
           <div>
-            <label htmlFor="email">Email:</label>
+            <label htmlFor='email'>Email:</label>
             <input
-              type="email"
-              id="email"
+              type='email'
+              id='email'
               value={email}
               onChange={(event) => setEmail(event.target.value)}
               style={styles.input}
             />
           </div>
           <div>
-            <label htmlFor="username">Enter UCID:</label>
+            <label htmlFor='username'>Enter UCID:</label>
             <input
-              type="text"
-              id="username"
+              type='text'
+              id='username'
               value={username}
               onChange={handleUsernameChange}
               style={styles.input}
             />
           </div>
           <div>
-            <label htmlFor="password">Create Password:</label>
+            <label htmlFor='password'>Create Password:</label>
             <input
-              type="password"
-              id="password"
+              type='password'
+              id='password'
               value={password}
               onChange={handlePasswordChange}
               style={styles.input}
@@ -118,21 +120,21 @@ function LoginForm() {
         </div>
         <div>
           <button
+            className='login-button'
             onClick={() => {
               handleLogout();
-              setError("");
-              setSuccess("");
+              setError('');
+              setSuccess('');
             }}
-            style={styles.buttonStyles}
           >
             Back
           </button>
           <button
-            type="submit"
-            style={styles.buttonStyles}
+            className='login-button'
+            type='submit'
             onClick={() => {
-              setError("");
-              setSuccess("");
+              setError('');
+              setSuccess('');
             }}
           >
             Create
@@ -140,16 +142,16 @@ function LoginForm() {
         </div>
         <div
           style={{
-            fontSize: "15px",
-            color: "green",
+            fontSize: '15px',
+            color: 'green',
           }}
         >
           {success}
         </div>
         <div
           style={{
-            fontSize: "15px",
-            color: "red",
+            fontSize: '15px',
+            color: 'red',
           }}
         >
           {error}
@@ -161,47 +163,47 @@ function LoginForm() {
 
 const styles = {
   buttonStyles: {
-    width: "60px",
-    margin: "10px",
+    width: '60px',
+    margin: '10px',
   },
   errorMess: {
-    fontSize: "10px",
-    color: "red",
+    fontSize: '10px',
+    color: 'red',
   },
   appBackground: {
-    color: "#db471a",
+    color: '#db471a',
   },
   login: {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    backgroundColor: "#F08000",
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#F08000',
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     lineHeight: 2.0,
-    color: "#292b2c",
+    color: '#292b2c',
     borderRadius: 30,
-    borderWidth: "thick",
-    width: "35%",
-    height: "250px",
-    height: "400px",
-    overflow: "scroll",
+    borderWidth: 'thick',
+    width: '35%',
+    height: '250px',
+    height: '400px',
+    overflow: 'scroll',
   },
   username: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "10px",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '10px',
   },
   input: {
-    boxSizing: "border-box",
-    width: "100%",
-    padding: "3px",
-    borderRadius: "5px",
-    border: "1px solid #ccc",
-    outline: "none",
+    boxSizing: 'border-box',
+    width: '100%',
+    padding: '3px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    outline: 'none',
   },
 };
 
